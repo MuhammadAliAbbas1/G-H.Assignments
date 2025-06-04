15a0b3c9b7ac011dde2181df8e2f81d28eb1d432
MuhammadAliAbbas1 <kmahrukh872@gmail.com>
2025-06-04 22:53:35 +0500

    first commit

---
+++ Assignments/Python Assignments/Assignment 4/00_intro_python/01_add_two_numbers.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+def main():
+    st.title("🔢 Sum Calculator")
+
+    num1 = st.number_input("Enter the first number:", step=1)
+    num2 = st.number_input("Enter the second number:", step=1)
+
+    if st.button("Calculate Sum"):
+        total = num1 + num2
+        st.success(f"The sum of {num1} and {num2} is: {total}")
+
+main()
+
